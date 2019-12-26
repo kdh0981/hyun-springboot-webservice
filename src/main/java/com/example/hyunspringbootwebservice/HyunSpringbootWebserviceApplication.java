6fa5e40e62b5749e4c3fb0c233cc7a64ae8aca21
kdh0981 <geniushyun90@gmail.com>
2019-12-26 20:38:54 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/hyunspringbootwebservice/HyunSpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyunspringbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyunSpringbootWebserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HyunSpringbootWebserviceApplication.class, args);
+  }
+
+}
