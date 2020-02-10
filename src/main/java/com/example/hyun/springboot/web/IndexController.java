58fcb8e0a1c902d81b9d5b3f512ee26baa050e6c
kdh0981 <geniushyun90@gmail.com>
2020-02-10 16:07:55 +0900

    LoginSession 어노테이션 형식으로 변경

--- src/main/java/com/example/hyun/springboot/web/IndexController.java
+++ src/main/java/com/example/hyun/springboot/web/IndexController.java
@@ -1,7 +1,7 @@
 package com.example.hyun.springboot.web;
 
+import com.example.hyun.springboot.config.auth.LoginUser;
 import com.example.hyun.springboot.config.auth.dto.SessionUser;
-import com.example.hyun.springboot.domain.user.User;
 import com.example.hyun.springboot.service.posts.PostsService;
 import com.example.hyun.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -10,19 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
   private final PostsService postsService;
-  private final HttpSession httpSession;
 
   @GetMapping("/")
-  public String index(Model model) {
+  public String index(Model model, @LoginUser SessionUser user) {
     model.addAttribute("posts", postsService.findAllDesc());
 
-    SessionUser user = (SessionUser) httpSession.getAttribute("user");
     if ( user != null ) {
       model.addAttribute("userName", user.getName());
     }
